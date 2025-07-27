5fd601cd4e7a0711d6ba23b5b928716a9ef1a9b9
Romain Rabouan <romain.rabouan@withings.com>
2025-07-27 18:20:36 +0200

    Add DailyAccumulator

--- Common/Core/WeatherModel/WeatherCondition.swift
+++ Common/Core/WeatherModel/WeatherCondition.swift
@@ -18,3 +18,27 @@ enum WeatherCondition: Int {
     case atmosphere
     case unknown
 }
+
+extension WeatherCondition {
+    /// See `DailyAccumulator.swift` for more info and the use of this property.
+    var priority: Int {
+        switch self {
+        case .unknown:
+            return 0
+        case .clear:
+            return 1
+        case .clouds:
+            return 2
+        case .rain:
+            return 3
+        case .snow:
+            return 4
+        case .thunderstorm:
+            return 5
+        case .drizzle:
+            return 6
+        case .atmosphere:
+            return 7
+        }
+    }
+}
